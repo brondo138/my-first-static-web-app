3c60d137f1077d7266def52033677e2934ba464b
brondo138 <lovosalex0@gmail.com>
2025-11-30 22:23:05 +0100

    Primer commit: proyecto inicial para Azure Static Web Apps

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btnSaludo").addEventListener("click", () => {
+    const mensaje = document.getElementById("mensaje");
+    mensaje.innerText = "\n¡Hola desde JavaScript! Nuestro despliegue en Azure funciona perfecto";
+});
